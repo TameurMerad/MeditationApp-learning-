af1007b455d481a99aafec729c2582d0439a8f64
Soufyan <tameur.merad@gmail.com>
2023-10-18 03:22:53 +0100

    update

--- app/src/main/java/com/example/composefirst/MainActivity.kt
+++ app/src/main/java/com/example/composefirst/MainActivity.kt
@@ -24,25 +24,37 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,17 +64,28 @@ import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.composefirst.ui.theme.ComposeFirstTheme
+import com.example.composefirst.ui.theme.values
+import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import java.time.format.TextStyle
 
 
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var painter = "https://i.pinimg.com/originals/5a/dd/33/5add3332302c9db5e9a6aeedfeb6b29b.jpg"
-            var nigger = "https://images.unsplash.com/photo-1696960594920-1ca9a1f250bc?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1972"
-            val title = "this is so me"
+            val textFieldState = remember{ mutableStateOf("") }
+            val scaffoldStat = rememberScrollState()
+            val scope = rememberCoroutineScope()
+            val snackBarHostState = remember {SnackbarHostState()}
+            val keyboardController = LocalSoftwareKeyboardController.current
+            Scaffold (
+                snackbarHost = { SnackbarHost(hostState = snackBarHostState)},
+                modifier = Modifier.fillMaxSize()
+            ){
                 Column(Modifier.verticalScroll(rememberScrollState())) {
                     Row(
                         modifier = Modifier
@@ -71,26 +94,69 @@ class MainActivity : ComponentActivity() {
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         val context = LocalContext.current
-
-                        ImageCard(myImageUrl = painter, title = title, modifier = Modifier
+                        ImageCard(myImageUrl = values.painter, title = values.title, modifier = Modifier
                             .weight(1f)
                             .padding(8.dp),
-                            secondImg = nigger
+                            secondImg = values.nigger
                         )
-                        AsyncImage(
-                            model = ImageRequest.Builder(LocalContext.current.applicationContext)
-                                .data(painter)
-                                .crossfade(true)
-                                .build(),
-                             contentDescription = "pic",
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(8.dp)
-                            )
                     }
+
+                    OutlinedTextField(
+                        value = textFieldState.value,
+                        onValueChange = {textFieldState.value = it},
+                        label = {
+                            Text(text = "write here")
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(17.dp)
+                    )
+                    Box (
+
+                        modifier= Modifier
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = {
+                            keyboardController?.hide()
+//                            Toast.makeText(applicationContext,"hello nigger ${textFieldState.value}",Toast.LENGTH_SHORT).show()
+                            scope.launch {
+                                val result = snackBarHostState.showSnackbar(
+                                    "yasseur no9ch",
+                                    withDismissAction = true,
+                                    actionLabel = "retry",
+                                    duration = SnackbarDuration.Indefinite
+                                    )
+                                when(result){
+                                    SnackbarResult.Dismissed ->{
+                                        Toast.makeText(applicationContext,"action dismissed",Toast.LENGTH_SHORT).show()
+                                    }
+                                    SnackbarResult.ActionPerformed ->{
+                                        Toast.makeText(applicationContext,"action Performed",Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                            }
+
+                        }){
+                            Text(text = "click her nigger")
+                        }
+                    }
+
+
+
+
+
                 }
+
+            }
+
         }
     }
+
+
+
+
+
 
     @SuppressLint("UnrememberedMutableState")
     @Composable
@@ -108,23 +174,22 @@ class MainActivity : ComponentActivity() {
             {
 //            here is the image
                 AsyncImage(
-
                     contentDescription = "null",
                     modifier = Modifier
                         .fillMaxSize()
                         .background(Color.Transparent)
                         .clickable {
                             try {
-                                if (imageUrlT3i.value == secondImg){
+                                if (imageUrlT3i.value == secondImg) {
                                     imageUrlT3i.value = myImageUrl
-                                }else imageUrlT3i.value = secondImg
-
-
-                                Toast.makeText(applicationContext,"clicked",Toast.LENGTH_SHORT).show()
-
-                            }catch (e:Exception){
-                                Toast.makeText(applicationContext,e.message,Toast.LENGTH_SHORT).show()
-
+                                } else imageUrlT3i.value = secondImg
+                                Toast
+                                    .makeText(applicationContext, "clicked", Toast.LENGTH_SHORT)
+                                    .show()
+                            } catch (e: Exception) {
+                                Toast
+                                    .makeText(applicationContext, e.message, Toast.LENGTH_SHORT)
+                                    .show()
                             }
                         },
                     model = ImageRequest.Builder(LocalContext.current.applicationContext)
---
+++ app/src/main/java/com/example/composefirst/ui/theme/values.kt
@@ -0,0 +1,8 @@
+package com.example.composefirst.ui.theme
+
+object values {
+
+    val painter = "https://i.pinimg.com/originals/5a/dd/33/5add3332302c9db5e9a6aeedfeb6b29b.jpg"
+    val nigger = "https://images.unsplash.com/photo-1696960594920-1ca9a1f250bc?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1972"
+    val title = "this is so me"
+}
