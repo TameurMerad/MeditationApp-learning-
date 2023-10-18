282e940668896a174571a4f12637d9f7761c1bd4
Soufyan <tameur.merad@gmail.com>
2023-10-18 02:19:41 +0100

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -5,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.composefirst"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.composefirst"
@@ -51,7 +53,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -66,4 +68,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+
 }
--- app/src/main/java/com/example/composefirst/MainActivity.kt
+++ app/src/main/java/com/example/composefirst/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composefirst
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
@@ -31,6 +32,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
@@ -43,144 +47,127 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.composefirst.ui.theme.ComposeFirstTheme
 import java.time.format.TextStyle
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(R.drawable.pic)
+            var painter = "https://i.pinimg.com/originals/5a/dd/33/5add3332302c9db5e9a6aeedfeb6b29b.jpg"
+            var nigger = "https://images.unsplash.com/photo-1696960594920-1ca9a1f250bc?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1972"
             val title = "this is so me"
+                Column(Modifier.verticalScroll(rememberScrollState())) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        val context = LocalContext.current
 
-            Column (Modifier.verticalScroll(rememberScrollState())){
-                Row ( modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween){
-                    val context = LocalContext.current
-
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp)
-                        .clickable {
-                            Toast
-                                .makeText(context, "nmi nmi nmi ", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                    )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
+                        ImageCard(myImageUrl = painter, title = title, modifier = Modifier
+                            .weight(1f)
+                            .padding(8.dp),
+                            secondImg = nigger
+                        )
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current.applicationContext)
+                                .data(painter)
+                                .crossfade(true)
+                                .build(),
+                             contentDescription = "pic",
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(8.dp)
+                            )
+                    }
                 }
-
-
-                Row (modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),horizontalArrangement = Arrangement.SpaceBetween){
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f).fillMaxWidth()
-                        .padding(8.dp) )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp).fillMaxWidth() )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                }
-                Row ( modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween){
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                }
-                Row ( modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween){
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                }
-                Row ( modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween){
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                    ImageCard(painter = painter, title =title,modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp) )
-                }
-
-
-            }
-
-
-
-
-
         }
     }
-}
 
-@Composable
-fun ImageCard(painter: Painter, title: String, modifier: Modifier = Modifier) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(20.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 15.dp)
-    )
-    {
-        Box(modifier = Modifier.height(200.dp))
+    @SuppressLint("UnrememberedMutableState")
+    @Composable
+    fun ImageCard(myImageUrl:String, title: String, modifier: Modifier = Modifier,secondImg:String) {
+        val imageUrlT3i = mutableStateOf(myImageUrl)
+        Card(
+            modifier = modifier.fillMaxWidth()
+            ,
+            shape = RoundedCornerShape(20.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 15.dp)
+
+        )
         {
+            Box(modifier = Modifier.height(200.dp))
+            {
 //            here is the image
-            Image(
-                painter = painter,
-                contentDescription = "null",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
+                AsyncImage(
+
+                    contentDescription = "null",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Transparent)
+                        .clickable {
+                            try {
+                                if (imageUrlT3i.value == secondImg){
+                                    imageUrlT3i.value = myImageUrl
+                                }else imageUrlT3i.value = secondImg
+
+
+                                Toast.makeText(applicationContext,"clicked",Toast.LENGTH_SHORT).show()
+
+                            }catch (e:Exception){
+                                Toast.makeText(applicationContext,e.message,Toast.LENGTH_SHORT).show()
+
+                            }
+                        },
+                    model = ImageRequest.Builder(LocalContext.current.applicationContext)
+                        .data(imageUrlT3i.value)
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(R.drawable.nigga_man),
+                    contentScale = ContentScale.Crop
+                )
+
 
 //            here is the gradient
-            Box (modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ), startY = 400f
-                    )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ), startY = 400f
+                            )
+                        )
                 )
-            )
 
 //             here is the text
-            Box (modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),contentAlignment = Alignment.BottomStart)
-            {
-                Text(text = title,
-                    style = androidx.compose.ui.text.TextStyle(color = Color.White,
-                        fontSize = 16.sp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp), contentAlignment = Alignment.BottomStart
+                )
+                {
+                    Text(
+                        text = title,
+                        style = androidx.compose.ui.text.TextStyle(
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
+                    )
+                }
             }
         }
     }
-}
-@Preview(showBackground = true)
-@Composable
-fun CardPreview(){
-    ImageCard(painter = painterResource(id = R.drawable.pic), title ="hadi hia" )
+
+
 }
