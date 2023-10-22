752222bade00df0f02ae98d043aa4442ea97f738
Soufyan <tameur.merad@gmail.com>
2023-10-22 01:31:25 +0100

    gradient added

--- app/src/main/java/com/example/composefirst/ui/theme/meditation_app/HomeScreen.kt
+++ app/src/main/java/com/example/composefirst/ui/theme/meditation_app/HomeScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -43,6 +45,20 @@ fun HomeScreen(){
             BigText(text = "Featured")
             CardScroll()
     }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ), startY = 100f
+                )
+            )
+    )
+
     Box(modifier = Modifier
         .fillMaxSize(),
         contentAlignment = Alignment.BottomCenter
